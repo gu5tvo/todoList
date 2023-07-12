3b25dbe080535f956d5cf2cc5b8f7986811bea9d
gu5tvo <gustavpassinho@gmail.com>
2023-07-12 16:34:14 -0300

    fix: justifying the current itens

--- src/Components/AppBar/index.jsx
+++ src/Components/AppBar/index.jsx
@@ -8,14 +8,6 @@ import {
 export function AppBar() {
   return (
     <>
-      <StatusBar
-        bg='#3700B3'
-        barStyle='light-content'
-      />
-      <Box
-        safeAreaTop
-        bg='violet.600'
-      />
       <HStack
         bg='indigo.600'
         px='1'
@@ -48,27 +40,7 @@ export function AppBar() {
             icon={
               <Icon
                 as={MaterialIcons}
-                name='favorite'
-                size='sm'
-                color='white'
-              />
-            }
-          />
-          <IconButton
-            icon={
-              <Icon
-                as={MaterialIcons}
                 name='search'
-                size='sm'
-                color='white'
-              />
-            }
-          />
-          <IconButton
-            icon={
-              <Icon
-                as={MaterialIcons}
-                name='more-vert'
                 size='sm'
                 color='white'
               />
--- src/Screens/Home/index.jsx
+++ src/Screens/Home/index.jsx
@@ -45,6 +45,7 @@ export function Home() {
         w='100%'
         h='100%'
         justifyContent='space-between'
+        alignItems='center'
       >
         <AppBar />
         <Fab
